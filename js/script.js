347a86d9f3fbecf9aa141ba3340b7a2553cfec8a
chain <klaus.mika1052@gmail.com>
2023-04-04 19:40:17 -0300

    finalizado puxar dados da api e icluir nos comentários

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ import initScrollSmooth from "./modules/scroll-smooth.js";
 import initScrollAnimation from "./modules/scroll-animation.js";
 import initOpenStatus from "./modules/open-status.js";
 import { SlideNav } from "./modules/slide.js";
+import initFetchFeedback from "./modules/fetch-feedback.js";
 
 changeTitle();
 initScrollSmooth();
 initScrollAnimation();
 initOpenStatus();
+initFetchFeedback();
 
 const slide = new SlideNav(".slide-wrapper", ".slide");
 slide.init();
